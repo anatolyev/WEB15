179cd7f08c5e0b45f8e570e0a4b2900d7cc67b19
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-02-10 16:29:54 +0300

    Добавили все аргументы к переводу в том числе сохранение.

--- 0210_WEB4/ex2.py
+++ 0210_WEB4/ex2.py
@@ -1,8 +1,15 @@
 import argparse
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("all_args", metavar="Двоичные числа",
                     nargs='+', help="Двоичные числа будут преобразованы")
+parser.add_argument("--base", default=2, type=int,
+                    help="Система счисления по умолчанию")
+parser.add_argument("--log", default=sys.stdout,
+                    type=argparse.FileType('w'),
+                    help="Нужно ввести имя файла для конвертированных значений")
 arrgs = parser.parse_args()
-result = " ".join(map(lambda x: str(int(x, 2)), arrgs.all_args))
+result = " ".join(map(lambda x: str(int(x, arrgs.base)), arrgs.all_args))
+arrgs.log.write(result + "\n")
 print(result)
