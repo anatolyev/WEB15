fb0a2b0462dc2889db7ac53769261fee7c7c3988
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-02-10 16:20:59 +0300

    Переписали перевод двоичных чисел

--- 0210_WEB4/ex2.py
+++ 0210_WEB4/ex2.py
@@ -1,6 +1,8 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("arg1")
+parser.add_argument("all_args", metavar="Двоичные числа",
+                    nargs='+', help="Двоичные числа будут преобразованы")
 arrgs = parser.parse_args()
-print(arrgs.arg1)
+result = " ".join(map(lambda x: str(int(x, 2)), arrgs.all_args))
+print(result)
