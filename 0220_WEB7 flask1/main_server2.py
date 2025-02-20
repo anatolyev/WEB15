6f6472709a84320085cb27f341adcf487e493afa
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-02-20 16:36:59 +0300

    Добавлена страница с картинкой.

--- 0220_WEB7 flask1/main_server2.py
+++ 0220_WEB7 flask1/main_server2.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -14,6 +14,12 @@ def countdown():
     countdown_list = [str(x) for x in range(10, 0, -1)]
     countdown_list.append("Пуск!")
     return '<br>'.join(countdown_list)
+
+@app.route("/image")
+def image():
+    # http://127.0.0.1:8080/image
+    return f'''<img src="{url_for('static', filename='/images/image.jpg')}"
+               alt="Это сова!">'''
 
 
 if __name__ == '__main__':
