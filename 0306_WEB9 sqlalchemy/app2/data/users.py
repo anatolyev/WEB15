0d6a3737fa351745634fa08b0733c8456b89215d
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-03-06 17:38:53 +0300

    Доделали но без рабочего пароля.

--- 0306_WEB9 sqlalchemy/app2/data/users.py
+++ 0306_WEB9 sqlalchemy/app2/data/users.py
@@ -1,7 +1,6 @@
 import datetime
 import sqlalchemy
 from sqlalchemy import orm
-from werkzeug.security import generate_password_hash, check_password_hash
 
 from .db_session import SqlAlchemyBase
 
@@ -20,9 +19,3 @@ class User(SqlAlchemyBase):
 
     def __repr__(self):
         return f'<User> {self.id} {self.name} {self.email}'
-
-    def set_password(self, password):
-        self.hashed_password = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.hashed_password, password)
