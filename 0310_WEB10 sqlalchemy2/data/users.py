e704c7b2e645a6806adf3b2b18f855bf53805221
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-03-10 16:31:11 +0300

    Создана новая заготовка.

--- 0310_WEB10 sqlalchemy2/data/users.py
+++ 0310_WEB10 sqlalchemy2/data/users.py
@@ -16,7 +16,7 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
 
-    news = orm.relation("News", back_populates='user')
+    news = orm.relationship("News", back_populates='user')
 
     def __repr__(self):
         return f'<User> {self.id} {self.name} {self.email}'
