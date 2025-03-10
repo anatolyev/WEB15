bd16c5ebb29955dc4c1b3a818c14087864ed3a8e
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-03-10 16:20:43 +0300

    Добавили сессию для flask

--- 0310_WEB10 sqlalchemy2/main_ex_cookie.py
+++ 0310_WEB10 sqlalchemy2/main_ex_cookie.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, session
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandex_lycey_key'
 
 def main():
+    print("http://127.0.0.1:8080/session_flask")
     app.run(port=8080, host='127.0.0.1')
 
 @app.route("/")
@@ -13,11 +15,22 @@ def index():
     visit_count = int(request.cookies.get("visit_count", 0))
     if visit_count:
         res = make_response(f"Вы пришли на эту страницу {visit_count + 1} раз")
-        res.set_cookie("visit_count", str(visit_count + 1), max_age=60*60*24*365*2)
+        res.set_cookie("visit_count", str(visit_count + 1), max_age=1)
     else:
         res = make_response(f"Вы пришли на эту страницу в первый раз")
-        res.set_cookie("visit_count", "1", max_age=60*60*24*365*2)
+        res.set_cookie("visit_count", "1", max_age=60)
     return res
+
+
+@app.route("/")
+@app.route("/session_flask")
+def session_flask():
+    visit_count = session.get("visit_count", 0)
+    session['visit_count'] = visit_count + 1
+    session.permanent = True
+    if session['visit_count'] > 9:
+        session.pop('visit_count', None)
+    return make_response(f"Вы пришли на эту страницу {visit_count} раз")
 
 
 
