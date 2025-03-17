48897f6281698173fb150dc3c5ce06d91fee92f4
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-03-17 16:42:25 +0300

    Получили рабочий API

--- 0317_WEB12/main.py
+++ 0317_WEB12/main.py
@@ -12,8 +12,8 @@ api = Api(app, catch_all_404s=True)
 
 def main():
     db_session.global_init("db/blogs.db")
-    api.add_resource(news_resources.NewsListResource, 'api/v2/news')
-    api.add_resource(news_resources.NewsListResource, 'api/v2/news/<int:news_id>')
+    api.add_resource(news_resources.NewsListResource, '/api/v2/news')
+    api.add_resource(news_resources.NewsResource, '/api/v2/news/<int:news_id>')
     app.run(port=8080, host='127.0.0.1')
 
 if __name__ == '__main__':
