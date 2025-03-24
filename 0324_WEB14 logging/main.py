3042a598ed5d64b900280ebc6387366f98d5fc67
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-03-24 16:00:44 +0300

    Создано логирование. Певрый лог.

---
+++ 0324_WEB14 logging/main.py
@@ -0,0 +1,13 @@
+import logging
+
+def log():
+    i = 0
+    while i < 10:
+        logging.warning(i)
+        i += 1
+
+if __name__ == '__main__':
+    logging.basicConfig(filename="ex1.log")
+    log()
+
+
