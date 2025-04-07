043774a4f620064b38af22ac60b8f382d8abe05c
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-04-07 16:25:15 +0300

    Создан асинхронный повар

--- 0407_WEB16 async/ex1.py
+++ 0407_WEB16 async/ex1.py
@@ -1,26 +1,34 @@
 import time
 from datetime import datetime
+import asyncio
+import os
+
 
 COEFF = 1
 
-def dish(num, prepare, wait):
+async def dish(num, prepare, wait):
     print(f"Начинаем готовить в {datetime.now().strftime('%H:%M:%S')} "
           f"блюдо №{num} будем готовить {prepare} минут(ы).")
     time.sleep(COEFF * prepare)
     print(f"продолжаем в {datetime.now().strftime('%H:%M:%S')} "
           f"ждать {wait} минут(ы) пока приготовится блюдо №{num} .")
-    time.sleep(COEFF * wait)
+    await asyncio.sleep(COEFF * wait)
     print(f"Закончили готовить в {datetime.now().strftime('%H:%M:%S')} "
           f"блюдо №{num}.")
 
-def main():
-    dish(1, 2, 3)
-    dish(2, 5, 10)
-    dish(3, 3, 5)
+async def main():
+    task = [
+    asyncio.create_task(dish(2, 5, 10)),
+    asyncio.create_task(dish(3, 3, 5)),
+    asyncio.create_task(dish(1, 2, 3)),
+    ]
+    await asyncio.gather(*task)
 
 if __name__ == '__main__':
     t0 = time.time()
-    main()
+    if os.name == 'nt':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(main())
     delta = int((time.time() - t0) / COEFF)
     print(f"Все готово в {datetime.now().strftime('%H:%M:%S')} "
           f"было потрачено {delta} минут.")
