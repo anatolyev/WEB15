251d34a1ddd73732c7fbb8396db7e7c789a49b2b
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-04-10 16:14:08 +0300

    Создали эхо бот.

--- 0410_WEB17 python-telegram-bot/main.py
+++ 0410_WEB17 python-telegram-bot/main.py
@@ -11,5 +11,16 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+async def echo(update, context):
+    await update.message.reply_text(update.message.text)
 
+def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    text_handler = MessageHandler(filters.TEXT, echo)
+    application.add_handler(text_handler)
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
 
