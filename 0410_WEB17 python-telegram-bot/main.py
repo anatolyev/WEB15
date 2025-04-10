dc80918c768b0fe01a3de05892fbcc3f5a6030cd
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-04-10 17:04:47 +0300

    Попытка создать таймер. не удалась.

--- 0410_WEB17 python-telegram-bot/main.py
+++ 0410_WEB17 python-telegram-bot/main.py
@@ -1,6 +1,6 @@
 import logging
 from telegram.ext import Application, MessageHandler, filters, CommandHandler
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram import ReplyKeyboardMarkup
 from config import BOT_TOKEN
 
 # Enable logging
@@ -13,7 +13,8 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 async def keyboard1(update, context):
-    reply_keyboard = [['/start', '/help']]
+    reply_keyboard = [['/start', '/help'],
+                      ['/set 5', '/set 10', '/set 30', '/set 60', '/unset']]
     markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
     return markup
 
@@ -39,6 +40,53 @@ async def help_command(update, context):
     await update.message.reply_text("Я пока ничего не умею и даже не умею помогать... "
                                     "я только ваше эхо.")
 
+# Добавлены задачи
+async def alarm(context) -> None:
+    """Send the alarm message."""
+    job = context.job
+    await context.bot.send_message(job.chat_id, text=f"Beep! {job.data} seconds are over!")
+
+
+def remove_job_if_exists(name, context) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+async def set_timer(update, context) -> None:
+    """Add a job to the queue."""
+    chat_id = update.effective_message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = float(context.args[0])
+        if due < 0:
+            await update.effective_message.reply_text("Простите мы не можем вернуться в прошлое!")
+            return
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_once(alarm, due, chat_id=chat_id, name=str(chat_id), data=due)
+
+        text = "Таймер установлен!"
+        if job_removed:
+            text += " Предыдушей таймер удален."
+        await update.effective_message.reply_text(text)
+
+    except (IndexError, ValueError):
+        await update.effective_message.reply_text("Используйте: /set <секунд>")
+
+
+async def unset(update, context) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = "Таймер удален!" if job_removed else "Нет активных таймеров."
+    await update.message.reply_text(text)
+
+
 
 
 def main():
@@ -47,6 +95,8 @@ def main():
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
 
+    application.add_handler(CommandHandler("set", set_timer, has_args=True))
+    application.add_handler(CommandHandler("unset", unset))
 
 
     # Обработка и запуск (в конце)
