46eaec57d666c8a03252149f0e303516dcb5b0bd
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-04-10 16:03:36 +0300

    Создан бот. Добавлено логирование.

---
+++ 0410_WEB17 python-telegram-bot/main.py
@@ -0,0 +1,15 @@
+import logging
+from telegram.ext import Application, MessageHandler, filters
+from config import BOT_TOKEN
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+
+
