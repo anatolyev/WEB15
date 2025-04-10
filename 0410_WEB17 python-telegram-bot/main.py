17a94930572e9a8ae66918d60b24bce1d39faeb7
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-04-10 16:38:38 +0300

    Создали первую клавиатуру.

--- 0410_WEB17 python-telegram-bot/main.py
+++ 0410_WEB17 python-telegram-bot/main.py
@@ -1,5 +1,6 @@
 import logging
 from telegram.ext import Application, MessageHandler, filters, CommandHandler
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from config import BOT_TOKEN
 
 # Enable logging
@@ -11,16 +12,25 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+async def keyboard1(update, context):
+    reply_keyboard = [['/start', '/help']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    return markup
+
+
+
 async def echo(update, context):
     await update.message.reply_text(update.message.text)
 
 
 async def start(update, context):
     user = update.effective_user
+    markup = await keyboard1(update, context)
+
     await update.message.reply_html(
         rf"Привет {user.mention_html()}! Я эхо-бот."
         rf"Напишите мне что-нибудь, и я пришлю это назад!",
-
+        reply_markup=markup
     )
 
 
