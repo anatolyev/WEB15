cddbe2d01a69e862ea2225fa18858e7cbdf8ac69
Анатольев Алексей <anatolyev@yandexlyceum.ru>
2025-04-17 16:24:31 +0300

    Добавили клавиатуру в aiogram, но кнопки еще не работают.

--- 0417_WEB18 aiogram/main.py
+++ 0417_WEB18 aiogram/main.py
@@ -6,15 +6,29 @@ from os import getenv
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
 # Bot token can be obtained via https://t.me/BotFather
-from config import BOT_TOKEN, API_KEY
+from config import BOT_TOKEN
+
+reply_keyboard = [[KeyboardButton(text='/address'), KeyboardButton(text='/phone')],
+                  [KeyboardButton(text='/site'), KeyboardButton(text='/work_time')],
+                  [KeyboardButton(text='/stop')],
+                  ]
+markup = ReplyKeyboardMarkup(keyboard=reply_keyboard,
+                             resize_keyboard=True,
+                             one_time_keyboard=False)
+
+
 
 # All handlers should be attached to the Router (or Dispatcher)
-
 dp = Dispatcher()
+
+
+@dp.message(Command('start'))
+async def start(message: Message):
+    await message.reply("Привет", reply_markup=markup)
 
 
 @dp.message(CommandStart())
@@ -48,11 +62,12 @@ async def echo_handler(message: Message) -> None:
 async def main() -> None:
     # Initialize Bot instance with default bot properties which will be passed to all API calls
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
     # And the run events dispatching
     await dp.start_polling(bot)
 
-
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    # Запускаем логирование
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO, stream=sys.stdout
+    )
     asyncio.run(main())
